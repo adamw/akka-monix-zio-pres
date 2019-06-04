49d81157740397e087fb7c0602372dd9d88deddc
adamw <adam@warski.org>
2019-06-04 09:38:51 +0200

    Code

--- core/src/main/scala/com/softwaremill/demo/UsingAkka.scala
+++ core/src/main/scala/com/softwaremill/demo/UsingAkka.scala
@@ -9,5 +9,22 @@ import scala.util.{Failure, Success}
 
 object UsingAkka {
 
+  //
+
+  type Commands = List[String]
+
+  class LaserCutterException(msg: String) extends Exception(msg)
+
+  case class DeviceConnection(id: String) {
+    def runCommand(command: String): Future[Unit] = Future {
+      if (command == "turtle up") {
+        throw new LaserCutterException("There's no turtle")
+      } else {
+        println(s"[$id] Processing $command")
+      }
+    }
+  }
+
+  //
 
 }
--- core/src/main/scala/com/softwaremill/demo/UsingZio.scala
+++ core/src/main/scala/com/softwaremill/demo/UsingZio.scala
@@ -4,5 +4,22 @@ import scalaz.zio._
 
 object UsingZio {
 
+  //
+
+  type Commands = List[String]
+
+  case class DeviceConnection(id: String) {
+    def runCommand(command: String): IO[LaserCutterException, Unit] = IO {
+      if (command == "turtle up") {
+        throw new LaserCutterException("There's no turtle")
+      } else {
+        println(s"[$id] Processing $command")
+      }
+    }.refineOrDie { case e: LaserCutterException => e }
+  }
+
+  class LaserCutterException(msg: String) extends Exception(msg)
+
+  //
 
 }
